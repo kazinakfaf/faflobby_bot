c15e50330f9c0d181bf6fcbc270f42629ffa0d88
KaziNak FAF <kazinakfaf@pm.me>
2021-12-02 06:45:01 +0300

    Fix exclude dotenv file

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 source ./.env
-rsync -va --exclude 'vendor'  --exclude '.git' --exclude 'tmp' --exclude 'data' --exclude 'env' --exclude 'Gemfile.lock' "$PWD/" $DEPLOY_SSH:$DEPLOY_DIRECTORY
+rsync -va --exclude 'vendor'  --exclude '.git' --exclude 'tmp' --exclude 'data' --exclude '.env' --exclude 'Gemfile.lock' "$PWD/" $DEPLOY_SSH:$DEPLOY_DIRECTORY
 ssh $DEPLOY_SSH "cd $DEPLOY_DIRECTORY; docker-compose up -d"
 ssh $DEPLOY_SSH "cd $DEPLOY_DIRECTORY; docker-compose restart ruby"
